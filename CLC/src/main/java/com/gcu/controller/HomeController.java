56f198d9471e3d8fae89a3e71a7afd3ddd0fd50c
MMcCormack3-GCU <113863051+MMcCormack3-GCU@users.noreply.github.com>
2022-09-23 02:16:13 -0500

    Made HOME
    Users need to navigate to http://localhost:8080/home/ to work; but the navs work.

--- CLC/src/main/java/com/gcu/controller/HomeController.java
+++ CLC/src/main/java/com/gcu/controller/HomeController.java
@@ -1,5 +1,52 @@
 package com.gcu.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.gcu.model.RegisterModel;
+import com.gcu.model.LoginModel;
+import com.gcu.model.HomeModel;
+
+
+@Controller
+@RequestMapping ("/home")
+public class HomeController {
+	/**
+	 * This is HOME
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/")
+	public String dispHome(Model homeMod) {
+		homeMod.addAttribute("Title", "Home");
+		homeMod.addAttribute("homeModel", new HomeModel());
+		return "home";
+	}
+	/**
+	 * This is REGISTER
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/register")
+	public String dispReg(Model dispReg) {
+		dispReg.addAttribute("Title", "Register");
+		dispReg.addAttribute("registerModel", new RegisterModel());
+		return "register";
+	}
+	/**
+	 * This is LOGIN
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/login")
+	public String dispLog(Model logMod) {
+		logMod.addAttribute("Title", "Register");
+		logMod.addAttribute("loginModel", new LoginModel());
+		return "login";
+	}
 }
