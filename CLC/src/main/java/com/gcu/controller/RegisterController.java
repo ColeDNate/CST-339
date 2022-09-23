d17d621f74b713ab9d9139b3e046d7d832d70309
ColeDNate <colednate@gmail.com>
2022-09-22 20:23:15 -0700

    Register & Home files created
    added register controller
    added home controller
    added register html
    added home html

---
+++ CLC/src/main/java/com/gcu/controller/RegisterController.java
@@ -0,0 +1,22 @@
+package com.gcu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gcu.model.EventModel;
+import com.gcu.model.LoginModel;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+}
