56e09955dd2dc924efca211d7270cd58b1922d87
MMcCormack3-GCU <113863051+MMcCormack3-GCU@users.noreply.github.com>
2022-10-15 16:59:09 -0500

    Dedicated Modules
    Created dedicated Controller/Model classes for Display, Update, and Delete modules. Also, added note to application.properties.

---
+++ CLC/src/main/java/com/gcu/controller/ServicesController.java
@@ -0,0 +1,10 @@
+package com.gcu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/scheduler/events")
+public class ServicesController {
+
+}
---
+++ CLC/src/main/java/com/gcu/model/ServicesModel.java
@@ -0,0 +1,15 @@
+package com.gcu.model;
+
+public class ServicesModel {
+	public void display() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+}
