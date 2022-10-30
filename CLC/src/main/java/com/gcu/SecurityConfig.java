015b6f6e934d93956f67c6c33b5805b9621b72ea
MMcCormack3-GCU <113863051+MMcCormack3-GCU@users.noreply.github.com>
2022-10-29 23:57:43 -0500

    SecurityConfig notes
    Added some comments to the SecurityConfig file.

--- CLC/src/main/java/com/gcu/SecurityConfig.java
+++ CLC/src/main/java/com/gcu/SecurityConfig.java
@@ -18,6 +18,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	PasswordEncoder passEnc; //Makes a cycle with itself... somehow. 
 	//1=2? Because a cycle should need a minimum of 2 - I think.
+	
+	/** ACTION MESSAGE:
+	 * Relying upon circular references is discouraged and they are prohibited by 
+	 * default. Update your application to remove the dependency cycle between beans. 
+	 * As a last resort, it may be possible to break the cycle automatically by 
+	 * setting spring.main.allow-circular-references to true.
+	 */
+	@Autowired //added just to make sure uBServe is Autowired, can *probably* be removed
 	UserBusinessService uBServe;
 	
 	@Bean
